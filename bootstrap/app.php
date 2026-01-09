fbae20549785e1bbe5f7e05ea9fdba31ffa8b915
nisarg <hammerbyte.nisarg@gmail.com>
2026-01-09 05:31:16 +0530

    https fix

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,22 +7,23 @@ use Illuminate\Foundation\Configuration\Middleware;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
-        web: __DIR__.'/../routes/web.php',
-        api: __DIR__.'/../routes/api.php',
-        commands: __DIR__.'/../routes/console.php',
-        health: '/up',
+        web: __DIR__ . "/../routes/web.php",
+        api: __DIR__ . "/../routes/api.php",
+        commands: __DIR__ . "/../routes/console.php",
+        health: "/up",
         then: function () {
-            Route::namespace('Manage')
-                ->prefix('manage')
-                ->name('manage.')
-                ->group(base_path('routes/manage.php'));
-        },
+            Route::namespace("Manage")
+                ->prefix("manage")
+                ->name("manage.")
+                ->group(base_path("routes/manage.php"));
+        }
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->trustProxies(at: "*");
         $middleware->alias([
-            'AdminUserCheck' => AdminUserCheck::class
+            "AdminUserCheck" => AdminUserCheck::class,
         ]);
-        $middleware->group('admin', [
+        $middleware->group("admin", [
             \Illuminate\Cookie\Middleware\EncryptCookies::class,
             \Illuminate\Cookie\Middleware\AddQueuedCookiesToResponse::class,
             \Illuminate\Session\Middleware\StartSession::class,
@@ -35,4 +36,5 @@ return Application::configure(basePath: dirname(__DIR__))
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
-    })->create();
+    })
+    ->create();

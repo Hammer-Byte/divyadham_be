789c9e99cb65dc88329fc0ee63440917b400c5da
Abhishek Jadav <abhijadav99@gmail.com>
2026-01-17 00:30:20 +0530

    Added WebPage View and admin can add new web pages

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\PagesController; 
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/{slug}', [PagesController::class, 'renderPage']);
+

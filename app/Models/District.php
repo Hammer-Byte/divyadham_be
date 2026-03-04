882b548bcb89208aeb7a8f3e354f9aaf25fbfc32
Abhishek Jadav <abhijadav99@gmail.com>
2026-03-05 02:27:37 +0530

    Added new Taluka table in database

--- app/Models/District.php
+++ app/Models/District.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use App\Models\State;
+use App\Models\Taluka;
 
 class District extends Model
 {
@@ -14,7 +15,16 @@ class District extends Model
         'state_id',
     ];
 
-    public function getState() {
-        return $this->hasOne(State::class, 'state_id','id');
+    public function getState()
+    {
+        return $this->hasOne(State::class, 'state_id', 'id');
+    }
+
+    /**
+     * Get the talukas for the district.
+     */
+    public function getTalukas()
+    {
+        return $this->hasMany(Taluka::class, 'district_id', 'id');
     }
 }

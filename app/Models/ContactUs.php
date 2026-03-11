edb7892679ebfe02918eb3f94c8c0fe850e3bcd6
Abhishek Jadav <abhijadav99@gmail.com>
2026-03-12 01:48:07 +0530

    Added new column in the Contact Us page and added Contact Us Inquiry page in the ADMIN

--- app/Models/ContactUs.php
+++ app/Models/ContactUs.php
@@ -6,9 +6,21 @@ use Illuminate\Database\Eloquent\Model;
 
 class ContactUs extends Model
 {
+    protected $table = 'contact_us';
+
     protected $fillable = [
         'user_id',
         'comment_subject',
         'comment_message',
+        'attended',
     ];
+
+    protected $casts = [
+        'attended' => 'integer',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
 }
